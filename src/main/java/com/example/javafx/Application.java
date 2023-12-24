253f28fcaab297d70648afcb0b846c110b922f42
OULAKBIR Ilham <86001779+Oulakbir@users.noreply.github.com>
2023-12-24 11:39:27 +0100

    Committing classes audio,logout,text,status....,app.java

--- src/main/java/com/example/javafx/Application.java
+++ src/main/java/com/example/javafx/Application.java
@@ -1,8 +1,11 @@
 package com.example.javafx;
 
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -10,12 +13,16 @@ public class Application extends javafx.application.Application {
     private static Stage primaryStageObj;
     @Override
     public void start(Stage stage) throws IOException {
-
+        primaryStageObj =stage;
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.getIcons().add(new Image(getClass().getClassLoader().getResource("img/logo.png").toString()));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+        stage.setResizable(false);
         stage.setTitle("N7gramme!");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(e -> Platform.exit());
 
     }
 
